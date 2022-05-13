fc824028db2964c2a63f735a1ff45051f9444d76
Daniele Maggiore <dmaggiore@sorint.it>
2022-05-13 08:42:14 +0200

    Commit di test

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,8 @@ import { about } from "../portfolio";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-function About() {
+function About({test}) {
+  console.log(test)
   return <section id="about" className="about">
     {about?.fullName && (<h1 className="about__title">Hi, I am <span>{about.fullName}</span></h1>)}
     {about?.role && (<h3 className="about__role">{about.role}</h3>)}
