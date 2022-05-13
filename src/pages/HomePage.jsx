db329613152c59232fe5e63bcb603db858aa92df
Daniele Maggiore <dmaggiore@sorint.it>
2022-05-13 08:52:11 +0200

    Added dark mode

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const HomePage = () => {
   const [darkMode, setDarkMode] = useState(false);
   return (
-    <div>
+    <div className={ darkMode ? 'dark' : ''}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <About darkMode={darkMode} />
       <Projects />
